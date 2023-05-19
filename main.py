f8bf46ade6c62b5bebaa301989a164af07d51219
Spencer <sthompson.dev@outlook.com>
2023-05-19 00:36:16 -0600

    Added the meat and potatoes of langchain

--- main.py
+++ main.py
@@ -13,6 +13,15 @@ import openai
 
 # Langchain
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.agents import initialize_agent
+
+# Tools
+from langchain.agents import Tool
 
 # Streamlit
 import streamlit as st
@@ -25,13 +34,73 @@ import streamlit as st
 
 def main():
     load_dotenv()
-    openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
 
     st.set_page_config(page_title = "My webpage", page_icon = ":tada:", layout = "wide")
     
     st.header("Ask your PDF 💬")
 
     pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    
+    # TODO
+
+    llm = ChatOpenAI(
+        openai_api_key = os.getenv("OPENAI_API_KEY"),
+        temperature = 0,
+        model_name = 'gpt-3.5-turbo'
+    )
+
+    embedding = OpenAIEmbeddings()
+
+    persist_directory = 'db'
+
+    vectordb = Chroma.from_documents(
+        documents = texts, # big TODO - Need to connect submitted pdf to texts
+        embedding = embedding,
+        persist_directory = persist_directory
+    )
+
+    retriever = RetrievalQA.from_chain_type(
+        llm = llm,
+        chain_type = "stuff",
+        retriever = vectordb.as_retriever()
+    )
+
+    tools = [Tool(
+        func = retriever.run,
+        description = "Use this tool to answer Any question, This tool can also be used for follwo up questions from the user.",
+        name = 'Lex Fridman DB'
+    )]
+
+    memory = ConversationBufferWindowMemory(
+        memory_key = "chat_history",  # important to align with agent prompt (below)
+        k = 5,
+        return_messages = True
+    )
+     
+    conversational_agent = initialize_agent(
+        agent='chat-conversational-react-description', 
+        tools=tools, 
+        llm=llm,
+        verbose=True,
+        max_iterations=2,
+        early_stopping_method="generate",
+        memory=memory,
+    )
+
+    sys_msg = """You are a helpful chatbot that answers the user's questions.
+    """
+
+    prompt = conversational_agent.agent.create_prompt(
+        system_message=sys_msg,
+        tools=tools
+    )
+    
+    conversational_agent.agent.llm_chain.prompt = prompt
+
+
 
 if __name__ == "__main__":
     main()
