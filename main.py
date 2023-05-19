a82ce85da1d2021aa9dd5693f69b9ed77c6f2f5e
Spencer <sthompson.dev@outlook.com>
2023-05-19 00:13:47 -0600

    started fleshing out

--- main.py
+++ main.py
@@ -1,3 +1,31 @@
+'''
+Usage via terminal: streamlit run main.py
+
+'''
+
+
 import os
 from dotenv import load_dotenv
 
+# Openai
+import openai
+
+# Langchain
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+import streamlit as st
+
+
+
+
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
